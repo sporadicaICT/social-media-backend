da311e31dcadcc896da9ce67dfa3ddb9ed3e7783
Islam <islamkwnd@gmail.com>
2022-05-14 17:58:24 +0000

    ci(gitpod): :bug: Cleaned dockerfile and db.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const {
   database: { username, password },
 } = require("./config");
-const sequelize = new Sequelize("social-media", username, password, {
+const sequelize = new Sequelize("social_media", username, password, {
   host: database.host,
   dialect: "mysql",
 });
