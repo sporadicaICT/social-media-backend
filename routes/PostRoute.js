76dc7e248cf5bfd513192166caeded7288247f55
fortune710 <fortune0208@yahoo.com>
2022-06-02 11:03:51 +0100

    added route to delete comments

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -9,7 +9,7 @@ const { AddRepost } = require("../controllers/AddRepost");
 
 const { AddComment } = require('../controllers/Comments/AddComment');
 const { GetComments } = require('../controllers/Comments/GetComments');
-const { DeleteComments } = require('../controllers/Comments/DeleteComment');
+const { DeleteComment } = require('../controllers/Comments/DeleteComment');
 
 //First route for testing purposes
 PostRouter.get('/', (req, res) => res.send("<h1>helooo from post</h1>"))
@@ -18,12 +18,13 @@ PostRouter.post("/addpost", AddPost);
 PostRouter.patch("/addlikes", AddLikes);
 PostRouter.delete("/deletelikes", DeleteLikes);
 PostRouter.post("/addrepost", AddRepost);
-PostRouter.get("/:id", GetPosts);
+PostRouter.get("/:id", GetPosts); //Gets Posts of a partticular user
+
 
 //To manage comments on the post
-PostRouter.patch('/addcomment', AddComment);
-PostRouter.get('/comments', GetComments);
-//PostRouter.delete('/deletecomment', DeleteComments)
+PostRouter.patch(':postid/addcomment', AddComment);
+PostRouter.get(':postid/comments', GetComments);
+PostRouter.delete(':postid/deletecomment/:id', DeleteComment)
 
 module.exports = {
   PostRouter: PostRouter,
