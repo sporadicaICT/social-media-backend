0e629101dde78e0a5a9607d3769b5549ca8aa787
fortune710 <fortune0208@yahoo.com>
2022-06-02 09:57:33 +0100

    added express validator to auth child routes

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,25 @@
 const SignIn = require("./Auth/SignIn").SignIn;
+const SignUp = require('./Auth/SignUp').SignUp; 
+
 const express = require("express");
+const { body } = require('express-validator');
+
 const AuthRouter = express.Router();
 
-AuthRouter.post("/sign-in", SignIn);
+AuthRouter.get('/', (req, res)=> res.send("<h1>In Auth</h1>"))
+
+AuthRouter.post("/sign-in",[
+  body('email').trim().isEmail(),
+  body('password').isLength({ min:5 })
+], (req, res) =>  SignIn(req, res) );
+
+AuthRouter.post("/sign-up", [
+  body('email').trim().isEmail(),
+  body('password').isLength({ min:5 }),
+  body('username').trim().isLength({ min:5 })
+
+], (req, res) =>  SignUp(req, res) )
+
 module.exports = {
   Auth: AuthRouter,
 };
