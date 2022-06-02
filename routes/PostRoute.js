81aa5c557f2e54936d90054ba85a9f02bbe1b611
fortune710 <fortune0208@yahoo.com>
2022-06-02 10:03:24 +0100

    just some little testing i did🥱

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -6,11 +6,24 @@ const { AddLikes } = require("../controllers/AddLikes");
 const { DeleteLikes } = require("../controllers/DeleteLikes");
 const { AddRepost } = require("../controllers/AddRepost");
 
+
+const { AddComment } = require('../controllers/Comments/AddComment');
+const { GetComments } = require('../controllers/Comments/GetComments');
+const { DeleteComments } = require('../controllers/Comments/DeleteComment');
+
+//First route for testing purposes
+PostRouter.get('/', (req, res) => res.send("<h1>helooo from post</h1>"))
+
 PostRouter.post("/addpost", AddPost);
 PostRouter.patch("/addlikes", AddLikes);
 PostRouter.delete("/deletelikes", DeleteLikes);
 PostRouter.post("/addrepost", AddRepost);
 PostRouter.get("/:id", GetPosts);
+
+//To manage comments on the post
+PostRouter.patch('/addcomment', AddComment);
+PostRouter.get('/comments', GetComments);
+//PostRouter.delete('/deletecomment', DeleteComments)
 
 module.exports = {
   PostRouter: PostRouter,
