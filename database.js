6f1dcb385746814437ec7ce0fdfd123b520911df
fortune710 <fortune0208@yahoo.com>
2022-06-06 10:17:24 +0100

    made changes for prod env

--- database.js
+++ database.js
@@ -1,8 +1,23 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('social-media','root','', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+var sequelize = undefined;
+
+if(process.env.HEROKU_POSTGRESQL_BRONZE_URL){
+
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+        host: process.env.DATABASE_URL || 'postgresql-aerodynamic-02839',
+        dialect: 'postgres',
+        protocol: 'postgres'
+    });
+
+
+} else {
+    sequelize = new Sequelize('social-media','root','', {
+        host: 'localhost',
+        dialect: 'mysql',
+    });
+}
+
+
 
 module.exports = {
     sequelize: sequelize,
